bafc795e868270f7860b5c10f88946483bf058a3
carol <awdrg1210@gmail.com>
2020-08-06 15:38:03 +0800

    update account api

--- api/resource/account.py
+++ api/resource/account.py
@@ -1,6 +1,5 @@
 """API for account."""
-from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 from cookies_pool.db.clients import RedisClient
 
@@ -8,13 +7,24 @@ from cookies_pool.db.clients import RedisClient
 class Account(Resource):
     """Account."""
 
+    def __init__(self, *args, **kwargs):
+        """Init."""
+        super().__init__(*args, **kwargs)
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument(
+            'account', type=str, required=True, help='required account'
+        )
+        self.parser.add_argument(
+            'password', type=str, required=True, help='required password'
+        )
+
     def post(self, website: str):
         """Add account/password.
 
         Args:
             website (str): website name, e.g. 'fb', 'ig'
         """
-        data = request.get_json()
+        data = self.parser.parse_args()
         account = data.get('account')
         password = data.get('password')
         params = {
@@ -30,3 +40,27 @@ class Account(Resource):
                 'message': 'Add account success',
                 'params': params
             }, 201
+
+    def put(self, website: str):
+        """Update account.
+
+        Args:
+            website (str): website name, e.g. 'fb', 'ig'
+        """
+        data = self.parser.parse_args()
+        account = data.get('account')
+        password = data.get('password')
+        params = {
+            'website': website,
+            'account': account,
+            'password': password,
+        }
+        accounts_db = RedisClient(type_='accounts', website=website)
+        if accounts_db.get(account):
+            accounts_db.set(account=account, value=password)
+            return {
+                'message': 'Update account success',
+                'params': params
+            }
+        else:
+            return {'message': 'Account not found!'}, 404
